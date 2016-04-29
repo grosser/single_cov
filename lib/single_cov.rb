3164941e298688ad848a420bfc8df108df76b695
Mickael Pham <mickael.pham@gmail.com>
2016-04-29 13:59:39 -0700

    Fall back to Coverage.result when SimpleCov was defined but not started

--- lib/single_cov.rb
+++ lib/single_cov.rb
@@ -94,11 +94,15 @@ module SingleCov
 
     # do not ask for coverage when SimpleCov already does or it conflicts
     def coverage_results
-      if defined?(SimpleCov)
+      if defined?(SimpleCov) && has_result?
         SimpleCov.instance_variable_get(:@result).original_result
       else
         Coverage.result
       end
+    end
+
+    def has_result?
+      !SimpleCov.instance_variable_get(:@result).nil?
     end
 
     # start recording before classes are loaded or nothing can be recorded
