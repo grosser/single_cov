c774a35edc1526435ea35bd09e8defde2c63cb54
Michael Grosser <michael@grosser.it>
2016-05-22 21:13:50 -0700

    v0.4.0

--- lib/single_cov.rb
+++ lib/single_cov.rb
@@ -122,6 +122,7 @@ module SingleCov
     end
 
     # do not record or verify when only running selected tests since it would be missing data
+    # either via option or via rails test runner which takes file:line
     def minitest_running_subset_of_tests?
       (ARGV & ['-n', '--name', '-l', '--line']).any? ||
         ARGV.first =~ /:\d+\Z/
